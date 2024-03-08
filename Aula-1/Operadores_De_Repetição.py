33adb354a9be32b9b3fec11beb5db4d5a0dad089
Dns <102502216+DNS239@users.noreply.github.com>
2024-03-07 23:55:02 -0300

    #

---
+++ Aula-1/Operadores_De_Repetição.py
@@ -0,0 +1,95 @@
+
+
+"""a = int(input("Informe um numero."))
+print(a)
+
+a += 1
+print(a)
+
+a += 1
+print(a)"""
+
+
+"""texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    if  letra.upper() in VOGAIS:
+        print(letra, end="")
+else:
+    print() # Adiciona uma quebra de linha.
+    print("Executa no final do laço")"""
+
+
+
+
+
+
+""""python"
+
+letra = P
+letra = Y
+letra = T
+letra = H
+letra = O
+letra = N"""
+
+
+
+
+"""for numero in range(0, 11):
+    print(numero, end=" ")
+
+print()
+
+for numero in range(0, 51, 5):
+    print(numero, end=" ")
+
+print()
+
+for numero in range(0, 101, 10):
+    print(numero, end=" ")
+
+print()
+
+for numero in range(0, 122, 11):
+    print(numero, end=" ")"""
+
+
+
+
+"""opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n: "))
+
+    if opcao == 1:
+        print("Saquando...")
+    elif opcao == 2:
+        print("Exibindo o Extrato...")
+else:
+    print("Obrigado por usar noso sistema bancario. Até logo!")"""
+
+
+
+
+
+while True:
+    numero = int(input("Informe um número: "))
+
+    if numero == 10:
+        break
+
+    if numero % 2 == 0:
+        continue
+    
+
+    print(numero)
+
+
+"""for numero in range(100):
+
+    if numero % 2 == 0:
+        continue
+
+    print(numero, end=" ")"""

6112808c9fbf4c3faa062f7e290a5f0d23b66934
Dns <102502216+DNS239@users.noreply.github.com>
2024-03-08 22:33:02 -0300

    #

---
+++ Aula-1/Metodos_Da_Classe_Lista.py
@@ -0,0 +1,58 @@
+"""lista = []
+
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista)
+"""
+
+
+"""lista = [1, "Python", [40, 30, 20]]
+
+print(lista)
+
+lista.clear()
+
+print(lista)
+"""
+
+
+
+
+
+"""lista = [1, "Python", [40, 30, 20]]
+
+l2 = lista.copy()
+
+print(lista)
+
+print(id(l2), id(lista))
+
+l2[0] = 2
+
+print(l2)
+print(lista)
+"""
+
+
+
+
+
+"""cores = ["vermelho", "azul", "verde", "azul"]
+
+cores.count("Vermeho")
+cores.count("Azul")
+cores.count("Verde")"""
+
+
+
+linguagem = ["Python", "js", "C"]
+
+print(linguagem)
+
+linguagem.extend(["java", "Csharp"])
+                 
+print(linguagem)
+

b74efaf8f64cdcc9287c4e787b340682a68441d3
Dns <102502216+DNS239@users.noreply.github.com>
2024-03-15 01:57:52 -0300

    #desafio feito

--- Aula-1/Loops.py
+++ Aula-1/Loops.py
@@ -19,3 +19,24 @@ carros = ["Audi", "Porshe", "Ferrari"]
 
 for indice, carros in enumerate(carros):
     print(f"{indice}: {carros}")
+
+
+
+
+
+
+# Solicita a entrada do usuário
+quantidade_passos = int(input(""))
+
+# Verifica se a quantidade de passos é positiva
+if quantidade_passos <= 0:
+    print("Nenhum passo dado na floresta.")
+else:
+    # Utiliza um loop for para simular os passos do explorador
+    for passo in range(1, quantidade_passos + 1):
+        if passo == 1:
+            print(f"Explorador: {passo} passo")
+        else:
+            print(f"Explorador: {passo} passos")
+
+

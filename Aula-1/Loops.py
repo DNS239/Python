6075c7760746c5c0a5a65c86c058ff0d53eeb4bf
Dns <102502216+DNS239@users.noreply.github.com>
2024-03-18 23:04:11 -0300

    #

--- Aula-1/Loops.py
+++ Aula-1/Loops.py
@@ -74,3 +74,15 @@ lista(itens)"""
 
 
 
+#                                Desafio 3                          #
+
+
+
+"""capacidade_atual, aumento_percentual = map(int, input().split())
+
+aumento_teraflops = (capacidade_atual * aumento_percentual) // 100
+
+nova_capacidade_total = capacidade_atual + aumento_teraflops
+
+print(int(nova_capacidade_total))
+"""

412c73f04a722f82fc0e66d10e3ca7ad35e900f5
Dns <senhordasestrelas1223@gmail.com>
2024-09-02 00:01:35 -0300

    #

--- python/03-ApiComFlask/dio_Bank/src/app.py
+++ python/03-ApiComFlask/dio_Bank/src/app.py
@@ -1,41 +1,72 @@
-from flask import Flask, url_for, request
-
-app = Flask(__name__)
+from flask import Flask
 
 
-@app.route("/olaMundo/<usuario>/<int:idade>/<float:altura>")
-def hello_world(usuario, idade, altura):
-    # print(idade)
-    # print(f"tipo da variavel idade: {type(idade)}")
-    # print(f"tipo da variavel usuario: {type(usuario)}")
-    # print(f"tipo da variavel altura: {type(altura)}")
-    return {
-        'Usuario': usuario,
-        'Idade': idade,
-        'Altura': altura,
-    }
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE="diobanck.sqlite",
+    )
 
-
-@app.route("/bemVindo")
-def bem_Vindo():
-    return {"message": "Óla, mundo"}
-
-
-@app.route("/projects/")
-def projects():
-    return "The project page"
-
-
-@app.route("/about", methods=["GET", "POST"])
-def about():
-    if request.method == "GET":
-        return "This is a GET"
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
     else:
-        return "This is a POST"
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # # ensure the instance folder exists
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
+
+    from . import db
+
+    db.init_app(app)
+
+    return app
 
 
-with app.test_request_context():
-    print(url_for("bem_Vindo"))
-    print(url_for("projects"))
-    print(url_for("about", next="/"))
-    print(url_for("hello_world", usuario="Dns", idade=28, altura=1.68))
+# from flask import Flask, url_for, request
+
+# app = Flask(__name__)
+
+
+# @app.route("/olaMundo/<usuario>/<int:idade>/<float:altura>")
+# def hello_world(usuario, idade, altura):
+#     # print(idade)
+#     # print(f"tipo da variavel idade: {type(idade)}")
+#     # print(f"tipo da variavel usuario: {type(usuario)}")
+#     # print(f"tipo da variavel altura: {type(altura)}")
+#     return {
+#         'Usuario': usuario,
+#         'Idade': idade,
+#         'Altura': altura,
+#     }
+
+
+# @app.route("/bemVindo")
+# def bem_Vindo():
+#     return {"message": "Óla, mundo"}
+
+
+# @app.route("/projects/")
+# def projects():
+#     return "The project page"
+
+
+# @app.route("/about", methods=["GET", "POST"])
+# def about():
+#     if request.method == "GET":
+#         return "This is a GET"
+#     else:
+#         return "This is a POST"
+
+
+# with app.test_request_context():
+#     print(url_for("bem_Vindo"))
+#     print(url_for("projects"))
+#     print(url_for("about", next="/"))
+#     print(url_for("hello_world", usuario="Dns", idade=28, altura=1.68))

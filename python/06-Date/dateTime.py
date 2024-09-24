e379551f060d02b6ba528a3bc83d38ee5ab30512
Dns <senhordasestrelas1223@gmail.com>
2024-09-24 19:18:37 -0300

    #

---
+++ python/06-Date/dateTime.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime, time
+
+date = date(2024, 9, 24)
+print(date)
+
+print(date.today())
+
+date_hora = datetime(2024, 9, 24, 18, 49, 00)
+print(date_hora)
+print(datetime.today())
+
+hora = time(10, 20, 0)
+print(hora)

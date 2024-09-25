23a2c5f44ce8ff99d4e4bdcd5b0feed5a2925198
Dns <senhordasestrelas1223@gmail.com>
2024-09-25 17:37:31 -0300

    #

--- python/06-Date/timeDate.py
+++ python/06-Date/timeDate.py
@@ -1,17 +1,43 @@
-from datetime import datetime, timedelta
+from datetime import date, datetime, time, timedelta
+
 
 tipo_carro = "M"  # P, M, G
+
 tempo_pequeno = 30
+
 tempo_medio = 45
+
 tempo_grande = 60
+
 data_atual = datetime.now()
 
+
 if tipo_carro == "p":
-    data_estimada = data_atual + timedelta(minutes=tempo_pequeno)
+
+    data_estimada = data_atual + timedelta(days=tempo_pequeno)
+
     print(f"O carro chegou: {data_atual} e fiacará pronto ás {data_estimada}")
+
 elif tipo_carro == "M":
-    data_estimada = data_atual + timedelta(minutes=tempo_medio)
+
+    data_estimada = data_atual + timedelta(days=tempo_medio)
+
     print(f"O carro chegou: {data_atual} e fiacará pronto ás {data_estimada}")
+
 else:
-    data_estimada = data_atual + timedelta(minutes=tempo_grande)
+
+    data_estimada = data_atual + timedelta(days=tempo_grande)
+
     print(f"O carro chegou: {data_atual} e fiacará pronto ás {data_estimada}")
+
+
+
+print(date.today() - timedelta(days=1))
+
+
+resultado = datetime(2023, 7, 25, 10, 19, 20) - timedelta(hours=1)
+
+print(resultado.time())
+
+
+print(datetime.now().date())

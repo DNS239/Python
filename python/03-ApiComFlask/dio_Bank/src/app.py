782481c71cab771a3e5931200f0e8a6e9ebe5a68
Dns <senhordasestrelas1223@gmail.com>
2024-11-26 23:21:00 -0300

    corrigido o caminho do banco de dados pra raiz do projeto

--- python/03-ApiComFlask/dio_Bank/src/app.py
+++ python/03-ApiComFlask/dio_Bank/src/app.py
@@ -1,14 +1,9 @@
 import os
-from datetime import datetime
 
-import click
-import sqlalchemy as sa
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
-
-
+from sqlalchemy.orm import DeclarativeBase
+import click
 
 
 class Base(DeclarativeBase):
@@ -17,27 +12,6 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
-    #email: Mapped[str] = mapped_column(sa.String)
-
-
-    def __repr__(self) -> str:
-        return f"User(id={self.id!r}, username={self.username!r})"
-    
-    
-class Post(db.Model):
-    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(sa.String, nullable=False)
-    body: Mapped[str] = mapped_column(sa.String, nullable=False)
-    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
-    author_id: Mapped[int] = mapped_column(sa.ForeignKey('user.id'))
-
-
-    def __repr__(self) -> str:
-        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
 
 @click.command("init-db")
 def init_db_command():
@@ -52,9 +26,10 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
-        SQLALCHEMY_DATABASE_URI="sqlite:///dio_bank.sqlite",
-    )
+    SECRET_KEY="dev",
+    SQLALCHEMY_DATABASE_URI=f"sqlite:///{os.path.abspath(os.path.join(os.getcwd(), 'Dio_Bank.sqlite'))}"
+)
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -63,17 +38,13 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # register cli commands
     app.cli.add_command(init_db_command)
-
-    # initialize extensions
     db.init_app(app)
+
+    # a simple page that says hello
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
 
     return app
 

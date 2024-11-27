53b035d7950ca7312d0e088a211cc023ff835738
Dns <senhordasestrelas1223@gmail.com>
2024-11-27 03:24:05 -0300

    Mapeamento dos dados feito

--- python/03-ApiComFlask/dio_Bank/src/app.py
+++ python/03-ApiComFlask/dio_Bank/src/app.py
@@ -1,9 +1,12 @@
 import os
-
+from datetime import datetime
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 import click
+from sqlalchemy import Integer, String
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -11,6 +14,25 @@ class Base(DeclarativeBase):
 
 
 db = SQLAlchemy(model_class=Base)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
 
 
 @click.command("init-db")
@@ -26,10 +48,9 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-    SECRET_KEY="dev",
-    SQLALCHEMY_DATABASE_URI=f"sqlite:///{os.path.abspath(os.path.join(os.getcwd(), 'Dio_Bank.sqlite'))}"
-)
-
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{os.path.abspath(os.path.join(os.getcwd(), 'Dio_Bank.sqlite'))}",
+    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
